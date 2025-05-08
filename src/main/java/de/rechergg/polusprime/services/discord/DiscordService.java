a8c92aaee7b982862c8498d32e38acced6bda4fa
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-05-08 18:26:11 +0200

    Add liveNotification method to DiscordService interface

--- src/main/java/de/rechergg/polusprime/services/discord/DiscordService.java
+++ src/main/java/de/rechergg/polusprime/services/discord/DiscordService.java
@@ -1,6 +1,7 @@
 package de.rechergg.polusprime.services.discord;
 
 import de.rechergg.polusprime.services.discord.commands.CommandManager;
+import de.rechergg.polusprime.services.discord.notification.DiscordLiveNotification;
 import net.dv8tion.jda.api.JDA;
 
 public interface DiscordService {
@@ -10,5 +11,7 @@ public interface DiscordService {
     JDA bot();
 
     CommandManager commandManager();
+
+    DiscordLiveNotification liveNotification();
 
 }
